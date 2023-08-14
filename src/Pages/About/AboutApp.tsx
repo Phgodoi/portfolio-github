3412e026353e099ea4a75f5496a5b39bb5c79241
Phgodoi <pauloh.godoi94@gmail.com>
2023-08-14 20:40:15 -0300

    sobre mim criado

--- src/Pages/About/AboutApp.tsx
+++ src/Pages/About/AboutApp.tsx
@@ -1,10 +1,10 @@
-import { Container, Title, TitleContent, TitleHighLight } from "./AboutStyle";
+import { Container, List, SubTitle, TitleContent, TitleHighLight } from "./AboutStyle";
 
 const Content = () => {
   return (
     <Container>
-      <TitleHighLight>Paulo Godoi</TitleHighLight>
-      <Title>Desenvolvedor Front-End</Title>
+      <TitleHighLight id="about">Paulo Godoi</TitleHighLight>
+      <SubTitle>Desenvolvedor Front-End</SubTitle>
 
       <TitleContent>
         Em busca de novas oportunidades na área de tecnologia como
@@ -22,8 +22,27 @@ const Content = () => {
         estou animado para encontrar uma equipe de trabalho que me permita
         crescer e evoluir como profissional.
       </TitleContent>
+
+      <SubTitle>Sobre Mim</SubTitle>
+      <TitleContent >
+      Formado em Gestão da Tecnologia da Informação pela Universidade Cruzeiro do Sul, foi durante o curso que tive meu primeiro contato com linguagens de programação. Desde então, me apaixonei pela área e venho estudando cada vez mais sobre o assunto. Além de possuir diversos cursos na área, como o <strong>Bootcamp Santander Full Stack Developer</strong> em parceria com a<strong> Dio - Digital Innovation One</strong>, também tenho aproveitado muito o conteúdo disponibilizado na plataforma do <strong>Curso em Vídeo</strong> com o <strong>professor Gustavo Guanabara</strong>.
+            Com todo esse conhecimento adquirido, comecei a desenvolver meu primeiro portfólio para demonstrar minhas habilidades e projetos desenvolvidos. Estou muito motivado em buscar novas oportunidades na área de tecnologia e aplicar meus conhecimentos em projetos desafiadores.
+
+            Tenho como objetivos imediatos encontrar uma oportunidade desafiadora na área de tecnologia, onde eu possa desenvolver minhas habilidades e aprimorar meu conhecimento. Busco por oportunidades como trainee ou desenvolvedor júnior, onde eu possa colocar em prática tudo o que aprendi até aqui e, ao mesmo tempo, continuar a aprender e crescer profissionalmente. Estou sempre em busca de aprimorar minhas habilidades e explorar novas tecnologias para oferecer as melhores soluções e resultados.
+      </TitleContent>
+
+      <SubTitle>Habilidades</SubTitle>
+            <List>
+                <li>HTML <i className="fa-brands fa-html5" /></li>
+                <li>CSS <i className="fa-brands fa-css3-alt" /></li>
+                <li>JavaScript  <i className="fa-brands fa-node-js" /> </li>
+                <li>TypeScript</li>
+                <li>React  <i className="fa-brands fa-react" /></li>
+                <li>Lógica de programação </li>
+                <li>Banco de dados <i className="fa-solid fa-database" /> </li>
+            </List>
     </Container>
   );
-};
+}; 
 
 export { Content };
--- src/Pages/About/AboutStyle.ts
+++ src/Pages/About/AboutStyle.ts
@@ -4,20 +4,34 @@ export const Container = styled.main`
   padding: 60px 0;
   padding-bottom: 20px;
   width: 80vw;
-  margin: 20px auto;
+  margin: 30px auto;
 `;
 
 export const TitleHighLight = styled.h1`
   font-size: 3rem;
-  margin-bottom: 30px;
+  margin: 30px 5px;
 `;
-export const Title = styled.h2`
+
+export const SubTitle = styled.h2`
   font-size: 2rem;
-  margin-bottom: 30px;
+  margin: 30px 5px;
 `;
 
 export const TitleContent = styled.p`
   font-size: 1.2rem;
   line-height: 2rem;
   font-weight: 300;
+`;
+export const List = styled.ul`
+  font-size: 1.1rem;
+  font-weight: 300;
+
+  display: grid;
+  grid-template-columns: repeat(3, auto);
+  grid-gap: 15px;
+  list-style: none;
+
+  li {
+    margin: 5px 10px;
+  }
 `;
